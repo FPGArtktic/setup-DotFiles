acca79d7bcf9e5d1d1d7c817302879f68d78ad56
Mateusz Okulanis <FPGArtktic@outlook.com>
2025-06-01 17:05:26 +0200

    fix: removing old garbage from priv repo

--- code-templates/hooks/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Skrypt pomocniczy do ręcznego uruchamiania deploymentu
-# Uruchamiany poprzez ./deploy.sh
-
-# Uruchom skrypt pre-push z parametrem 'manual'
-./hooks/pre-push manual
--- code-templates/hooks/setup-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Skrypt instalacyjny dla konfiguracji Git hooks
-# Uruchamiany poprzez ./setup-hooks.sh
-
-# Ustaw ścieżkę do hooków w konfiguracji Git
-git config core.hooksPath hooks
-
-echo "Konfiguracja Git hooks zakończona sukcesem. Hooki z katalogu 'hooks/' są teraz aktywne."
